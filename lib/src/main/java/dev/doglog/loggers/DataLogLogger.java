e291715cc418571fe4e4975a31c3d5e31a2c0fea
Jonah Snider <jonah@jonahsnider.com>
2024-04-27 04:46:22 -0700

    feat: reduce log file size with change detection (#8)
    * feat: reduce log file size with change detection
    
    * fix: fix crash from comparing hash against null

--- lib/src/main/java/dev/doglog/loggers/DataLogLogger.java
+++ lib/src/main/java/dev/doglog/loggers/DataLogLogger.java
@@ -20,6 +20,7 @@ import edu.wpi.first.util.datalog.StringLogEntry;
 import edu.wpi.first.util.datalog.StructArrayLogEntry;
 import edu.wpi.first.util.datalog.StructLogEntry;
 import edu.wpi.first.util.struct.Struct;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,6 +40,7 @@ public class DataLogLogger implements LogConsumer {
   private final Map<String, StringLogEntry> stringLogs = new HashMap<>();
   private final Map<String, StructArrayLogEntry<?>> structArrayLogs = new HashMap<>();
   private final Map<String, StructLogEntry<?>> structLogs = new HashMap<>();
+  private final Map<String, Integer> valueHashes = new HashMap<>();
 
   private final String logTable;
 
@@ -50,33 +52,68 @@ public class DataLogLogger implements LogConsumer {
   }
 
   public void log(String key, boolean[] value) {
-    booleanArrayLogs
-        .computeIfAbsent(key, k -> new BooleanArrayLogEntry(log, prefixKey(k)))
-        .append(value);
+    var hash = Arrays.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      booleanArrayLogs
+          .computeIfAbsent(key, k -> new BooleanArrayLogEntry(log, prefixKey(k)))
+          .append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, boolean value) {
-    booleanLogs.computeIfAbsent(key, k -> new BooleanLogEntry(log, prefixKey(k))).append(value);
+    var hash = Boolean.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      booleanLogs.computeIfAbsent(key, k -> new BooleanLogEntry(log, prefixKey(k))).append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, double[] value) {
-    doubleArrayLogs
-        .computeIfAbsent(key, k -> new DoubleArrayLogEntry(log, prefixKey(k)))
-        .append(value);
+    var hash = Arrays.hashCode(value);
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      doubleArrayLogs
+          .computeIfAbsent(key, k -> new DoubleArrayLogEntry(log, prefixKey(k)))
+          .append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, double value) {
-    doubleLogs.computeIfAbsent(key, k -> new DoubleLogEntry(log, prefixKey(k))).append(value);
+    var hash = Double.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      doubleLogs.computeIfAbsent(key, k -> new DoubleLogEntry(log, prefixKey(k))).append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, float[] value) {
-    floatArrayLogs
-        .computeIfAbsent(key, k -> new FloatArrayLogEntry(log, prefixKey(k)))
-        .append(value);
+    var hash = Arrays.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      floatArrayLogs
+          .computeIfAbsent(key, k -> new FloatArrayLogEntry(log, prefixKey(k)))
+          .append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, float value) {
-    floatLogs.computeIfAbsent(key, k -> new FloatLogEntry(log, prefixKey(k))).append(value);
+    var hash = Float.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      floatLogs.computeIfAbsent(key, k -> new FloatLogEntry(log, prefixKey(k))).append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, int[] value) {
@@ -84,13 +121,25 @@ public class DataLogLogger implements LogConsumer {
   }
 
   public void log(String key, long[] value) {
-    integerArrayLogs
-        .computeIfAbsent(key, k -> new IntegerArrayLogEntry(log, prefixKey(k)))
-        .append(value);
+    var hash = Arrays.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      integerArrayLogs
+          .computeIfAbsent(key, k -> new IntegerArrayLogEntry(log, prefixKey(k)))
+          .append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, int value) {
-    integerLogs.computeIfAbsent(key, k -> new IntegerLogEntry(log, prefixKey(k))).append(value);
+    var hash = Integer.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      integerLogs.computeIfAbsent(key, k -> new IntegerLogEntry(log, prefixKey(k))).append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   // TODO: Protobuf logs
@@ -101,41 +150,70 @@ public class DataLogLogger implements LogConsumer {
     if (value == null) {
       return;
     }
-    stringArrayLogs
-        .computeIfAbsent(key, k -> new StringArrayLogEntry(log, prefixKey(k)))
-        .append(value);
+
+    var hash = Arrays.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      stringArrayLogs
+          .computeIfAbsent(key, k -> new StringArrayLogEntry(log, prefixKey(k)))
+          .append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public void log(String key, String value) {
     if (value == null) {
       return;
     }
-    stringLogs.computeIfAbsent(key, k -> new StringLogEntry(log, prefixKey(k))).append(value);
+
+    var hash = value.hashCode();
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      stringLogs.computeIfAbsent(key, k -> new StringLogEntry(log, prefixKey(k))).append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public <T> void log(String key, Struct<T> struct, T[] value) {
     if (struct == null || value == null) {
       return;
     }
-    @SuppressWarnings("unchecked")
-    var entry =
-        (StructArrayLogEntry<T>)
-            structArrayLogs.computeIfAbsent(
-                key, k -> StructArrayLogEntry.create(log, prefixKey(k), struct));
 
-    entry.append(value);
+    var hash = Arrays.hashCode(value);
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      @SuppressWarnings("unchecked")
+      var entry =
+          (StructArrayLogEntry<T>)
+              structArrayLogs.computeIfAbsent(
+                  key, k -> StructArrayLogEntry.create(log, prefixKey(k), struct));
+
+      entry.append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   public <T> void log(String key, Struct<T> struct, T value) {
     if (struct == null || value == null) {
       return;
     }
-    @SuppressWarnings("unchecked")
-    var entry =
-        (StructLogEntry<T>)
-            structLogs.computeIfAbsent(key, k -> StructLogEntry.create(log, prefixKey(k), struct));
 
-    entry.append(value);
+    var hash = value.hashCode();
+
+    if (valueHashes.getOrDefault(key, 0) != hash) {
+      @SuppressWarnings("unchecked")
+      var entry =
+          (StructLogEntry<T>)
+              structLogs.computeIfAbsent(
+                  key, k -> StructLogEntry.create(log, prefixKey(k), struct));
+
+      entry.append(value);
+
+      valueHashes.put(key, hash);
+    }
   }
 
   private String prefixKey(String key) {
