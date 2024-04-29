ef04e3118d0ea8ee66783f138b427fced3a72f8c
Jonah Snider <jonah@jonahsnider.com>
2024-04-28 19:21:49 -0700

    docs: add package-info files

---
+++ lib/src/main/java/dev/doglog/internal/package-info.java
@@ -0,0 +1,2 @@
+/** Internal classes used by DogLog. 99% chance you don't want to use any of these directly. */
+package dev.doglog.internal;
---
+++ lib/src/main/java/dev/doglog/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * DogLog is a simple, unopinionated logging library for FRC.
+ *
+ * <p>See https://doglog.dev for more information.
+ */
+package dev.doglog;
