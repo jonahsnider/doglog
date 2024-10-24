8cd7d074b24c1cbaa82076ef52626296ded71d41
Jonah Snider <jonah@jonahsnider.com>
2024-10-23 19:59:02 -0700

    fix: improve performance of logging faults

--- lib/src/main/java/dev/doglog/internal/FaultLogger.java
+++ lib/src/main/java/dev/doglog/internal/FaultLogger.java
@@ -5,31 +5,35 @@
 package dev.doglog.internal;
 
 import edu.wpi.first.hal.HALUtil;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /** Provides the interface for logging faults. */
 public class FaultLogger {
   private static final Map<String, Integer> faultCounts = new HashMap<>();
+  private static String[] faultNames = new String[] {};
 
+  // TODO: Why can't we just log direct to DogLog class here? Either refactor or add comment
+  // explaining why.
   public static void logFault(LogQueuer logger, String faultName) {
-    faultCounts.merge(faultName, 1, Integer::sum);
+    var previousCount = faultCounts.get(faultName);
+    var newCount = previousCount == null ? 1 : previousCount + 1;
+    faultCounts.put(faultName, newCount);
 
-    log(logger);
+    var now = HALUtil.getFPGATime();
+
+    if (previousCount == null) {
+      // A new fault has been seen
+      faultNames = Arrays.copyOf(faultNames, faultNames.length + 1);
+      faultNames[faultNames.length - 1] = faultName;
+      logger.queueLog(now, "Faults/Seen", faultNames);
+    }
+    logger.queueLog(now, "Faults/Counts/" + faultName, newCount);
   }
 
   public static boolean faultsLogged() {
     return !faultCounts.isEmpty();
-  }
-
-  private static void log(LogQueuer logger) {
-    var now = HALUtil.getFPGATime();
-
-    for (var entry : faultCounts.entrySet()) {
-      logger.queueLog(now, "Faults/Counts/" + entry.getKey(), entry.getValue());
-    }
-
-    logger.queueLog(now, "Faults/Seen", faultCounts.keySet().toArray(String[]::new));
   }
 
   private FaultLogger() {}
