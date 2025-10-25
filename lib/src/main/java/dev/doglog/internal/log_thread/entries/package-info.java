5be34e8d41202b70f654d5a9b9a917abb3d30882
Jonah Snider <jonah@jonahsnider.com>
2025-10-25 14:32:19 -0700

    feat: remove log thread

--- lib/src/main/java/dev/doglog/internal/log_thread/entries/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Log entry classes. Rather than having a single generic class with a `Object value` field, we have
- * separate classes for each type of log entry. This allows the JVM to optimize the code better,
- * since casting from `Object` makes it harder for the JIT to work.
- */
-@NullMarked
-package dev.doglog.internal.log_thread.entries;
-
-import org.jspecify.annotations.NullMarked;
