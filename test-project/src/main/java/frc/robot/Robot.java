853b085fb8629742444bcd95fd8f43aab51cf7ca
Jonah Snider <jonah@jonahsnider.com>
2025-11-18 23:16:57 -0800

    feat!: update to WPILib 2026 (#139)

--- test-project/src/main/java/frc/robot/Robot.java
+++ test-project/src/main/java/frc/robot/Robot.java
@@ -81,7 +81,7 @@ public class Robot extends TimedRobot {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
     }
 
     DogLog.clearFault("ExampleFault");
