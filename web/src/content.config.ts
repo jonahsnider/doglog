6e1ddd674cf28a084f6c6aeb65630b1078d14f69
Jonah Snider <jonah@jonahsnider.com>
2026-08-26 22:49:17 -0700

    docs: use my fork of starlight-changelogs
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- web/src/content.config.ts
+++ web/src/content.config.ts
@@ -8,15 +8,9 @@ export const collections = {
 	changelogs: defineCollection({
 		loader: changelogsLoader([
 			{
-				provider: 'keep-a-changelog',
+				provider: 'conventional-changelog',
 				base: 'reference/changelog',
 				changelog: '../CHANGELOG.md',
-				extractDate: ({ title }) => {
-					const date = title.match(/\((\d{4}-\d{2}-\d{2})\)$/)?.[1];
-
-					return date ? new Date(`${date}T12:00:00Z`) : undefined;
-				},
-				process: ({ title }) => title.replace(/\s+\(\d{4}-\d{2}-\d{2}\)$/, ''),
 			},
 		]),
 	}),
